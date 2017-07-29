b68d74881f8aba68956b9d0d43330dccfdbea68c
carlos <carloslechado@correo.ugr.es>
2017-07-29 14:20:01 +0200

    Settings for static

--- transitweb_project/urls.py
+++ transitweb_project/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from registration.backends.simple.views import RegistrationView
+from django.conf import settings
 
 """
 class MyRegistrationView(RegistrationView):
@@ -15,6 +16,7 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('transitweb.urls')),
     url(r'^tinymce/', include('tinymce.urls')),
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
     #url(r'^accounts/register/$', MyRegistrationView.as_view(), name='registration_register'),
     #url(r'^accounts/', include('registration.backends.simple.urls')),
 )
